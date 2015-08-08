deafc62c2b38a1da09c8f197ce2f6fe9260409a0
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2015-08-08 08:56:21 +0200

    chore: initial commit of the project

---
+++ src/main/java/com/sandornemeth/App.java
@@ -0,0 +1,13 @@
+package com.sandornemeth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
