4bdb5c546f61518b02f6bdf92254c09ae029e56e
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2015-08-10 19:50:52 +0200

    format(CsvReporterConfigurationTest): reformat of CsvReporterConfigurationTest

--- src/test/java/com/sandornemeth/metrics/spring/autoconfigure/CsvReporterConfigurationTest.java
+++ src/test/java/com/sandornemeth/metrics/spring/autoconfigure/CsvReporterConfigurationTest.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (C) 2015 Sandor Nemeth (sandor.nemeth.1986@gmail.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.sandornemeth.metrics.spring.autoconfigure;
 
@@ -26,20 +24,24 @@ import static org.junit.Assert.assertThat;
 
 public class CsvReporterConfigurationTest extends AbstractMetricsAutoconfigurationTestSupport {
 
-  @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
   @Test
   public void shouldNotLoadACsvReporterIfNoneConfigured() {
     this.load(CsvReporterTestConfiguration.class);
   }
 
-  @Test public void shouldLoadACsvReporter() throws InterruptedException {
+  @Test
+  public void csvReporterShouldBeActivatedOnConfiguration() throws InterruptedException {
     String[] contents = temporaryFolder.getRoot().list();
     assertThat(contents.length, is(0));
     this.load(CsvReporterTestConfiguration.class, "spring.metrics.reporters.csv.enabled=true",
               "spring.metrics.reporters.csv.formatFor=US",
               "spring.metrics.reporters.csv.reportFolder=" + temporaryFolder.getRoot()
                   .getAbsolutePath());
+
+    // sleep a little bit and verify if report files are generated.
     Thread.sleep(2000);
     String[] loggedContents = temporaryFolder.getRoot().list((f, name) -> name.endsWith(".csv"));
     assertThat(loggedContents.length, Matchers.greaterThan(0));
@@ -47,6 +49,7 @@ public class CsvReporterConfigurationTest extends AbstractMetricsAutoconfigurati
 
   @Configuration
   protected static class CsvReporterTestConfiguration {
+
   }
 
 }
