bbc50b74682b5f4a1de04fc06eb328d02e1ffd04
Sandor Nemeth <sandor.nemeth.1986@gmail.com>
2015-08-25 20:01:15 +0200

    started to add documentation

--- src/main/java/com/sandornemeth/metrics/spring/autoconfigure/MetricsAutoConfiguration.java
+++ src/main/java/com/sandornemeth/metrics/spring/autoconfigure/MetricsAutoConfiguration.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (C) 2015 Sandor Nemeth (sandor.nemeth.1986@gmail.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.sandornemeth.metrics.spring.autoconfigure;
 
@@ -42,7 +40,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 /**
- *
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-configuration} class
+ * for configuring the metrics-spring library.
  */
 @Configuration
 @ConditionalOnClass({MetricRegistry.class})
@@ -74,7 +73,7 @@ public class MetricsAutoConfiguration {
       });
     }
 
-    private void  configureCsvReporters(MetricRegistry metricRegistry) {
+    private void configureCsvReporters(MetricRegistry metricRegistry) {
       metricsConfigurationProperties.getReporters().getCsv().forEach(r -> {
         CsvReporter reporter = CsvReporter.forRegistry(metricRegistry)
             .filter(MetricFilter.ALL)
